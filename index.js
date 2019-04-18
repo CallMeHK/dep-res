5710b9763862ebbdf41b15741d5a3bf05cc0a600
Tyler Gregg <hkrazy888@gmail.com>
2019-04-17 22:16:32 -0600

    feat: Added recursive method to resolve dependencies.  Does not handle circular deps.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const resolveDependencies = require("./dependency-resolver");
 
 const case1 = ["KittenService: CamelCaser", "CamelCaser: "];
+// Expected output:
+// "CamelCaser, KittenService"
 const case2 = [
   "KittenService: ",
   "Leetmeme: Cyberportal",
@@ -9,5 +11,16 @@ const case2 = [
   "Fraudstream: Leetmeme",
   "Ice: "
 ];
-
-console.log(resolveDependencies());
+// Expected output:
+// "KittenService, Ice, Cyberportal, Leetmeme, CamelCaser, Fraudstream"
+const case3 = [
+  "KittenService: ",
+  "Leetmeme: Cyberportal",
+  "Cyberportal: Ice",
+  "CamelCaser: KittenService",
+  "Fraudstream: ",
+  "Ice: Leetmeme"
+];
+// Expected output:
+// "KittenService, Ice, Cyberportal, Leetmeme, CamelCaser, Fraudstream"
+console.log("final: ", resolveDependencies(case3));
